38bb3c29cf04f91d38248d02829056ada4c41334
Hevanafa <dhevansafa4@gmail.com>
2025-12-07 11:20:07 +0700

    added basic SDL2 wrapper procedures

--- UNITS/SDL2Wrapper.pas
+++ UNITS/SDL2Wrapper.pas
@@ -4,6 +4,23 @@ unit SDL2Wrapper;
 
 interface
 
+const 
+  SDL_INIT_VIDEO = $00000020;
+  SDL_WINDOWPOS_CENTERED = $2FFF0000;
+  SDL_WINDOW_SHOWN = $00000004;
+
+type
+  PSDL_Window = pointer;
+  PSDL_Renderer = pointer;
+
+function SDL_Init(const flags: longword): longint; cdecl; external 'SDL2.dll';
+function SDL_CreateWindow(const title: PChar; const x, y, w, h: longint; const flags: longword); cdecl; external 'SDL2.dll';
+function SDL_CreateRenderer(const window: PSDL_Window; const index: longint; const flags: longword); cdecl; external 'SDL2.dll';
+procedure SDL_DestroyRenderer(const renderer: PSDL_Renderer); cdecl; external 'SDL2.dll';
+procedure SDL_DestroyWindow(const window: PSDL_Window); cdecl; external 'SDL2.dll';
+procedure SDL_Quit; cdecl; external 'SDL2.dll';
+procedure SDL_Delay(const ms: longword); cdecl; external 'SDL2.dll';
+
 implementation
 
 end.
--- game.pas
+++ game.pas
@@ -7,13 +7,21 @@ uses
 
 type
   TGame = object(TPosit92)
+  public
+    procedure loadAssets;
   end;
+
+procedure TGame.loadAssets;
+begin
+  { Load more assets here }
+end;
+
 
 var
   game: TGame;
 
 begin
   game.init;
+  { TODO: Add the rest of the method calls }
   readln
 end.
-
