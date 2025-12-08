548380f8d569af0e17fe07892e989819303cd0d9
Hevanafa <dhevansafa4@gmail.com>
2025-12-09 04:31:32 +0700

    added demo cleanup script

---
+++ DEMOS/sound/cleanup_demo.ps1
@@ -0,0 +1,18 @@
+# Removes all .o, .ppu, and .res files
+
+$files = Get-ChildItem -include *.o, *.ppu, *.res -file
+
+if ($files.count -eq 0) {
+  write-host "No .o, .ppu or *.res files found" -foregroundColor white
+} else {
+  write-host "Found $($files.count) file(s) to delete" -foregroundColor yellow
+
+  $files | foreach-object { write-host "  $($_.FullName)" }
+
+  # Assuming that no files are locked after compilation
+  $files | remove-item
+  write-host "Deleted $($files.count) file(s)" -foregroundColor cyan
+}
+
+cd ..\..\experimental
+& .\cleanup.ps1
